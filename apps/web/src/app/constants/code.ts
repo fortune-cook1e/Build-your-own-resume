964ec411fbab4408cebb954b0ff2de311c08239c
fortune_cookie <767077147@qq.com>
2024-02-07 21:47:57 +0800

    feat(web): axios typescript format response

---
+++ apps/web/src/app/constants/code.ts
@@ -0,0 +1,5 @@
+export enum ResponseCode {
+  Success = 0,
+  Fail = 2,
+  InvalidCredentials = 1000,
+}
--- apps/web/src/app/utils/request.ts
+++ apps/web/src/app/utils/request.ts
@@ -1,4 +1,12 @@
+import { ResponseCode } from '@/web/app/constants/code';
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import { redirect } from 'next/navigation';
+
+interface ApiResponse<T> {
+  data: T;
+  code: ResponseCode;
+  message?: string;
+}
 
 const request = axios.create({
   baseURL: '/api',
@@ -15,11 +23,24 @@ request.interceptors.request.use(
 );
 
 request.interceptors.response.use(
-  function (response: AxiosResponse) {
-    console.log({ response });
+  function (response: AxiosResponse<ApiResponse<any>>) {
+    const { data } = response;
+    switch (data.code) {
+      case ResponseCode.Success: {
+        return data.data;
+      }
+      case ResponseCode.Fail: {
+        return Promise.reject(data);
+      }
+      case ResponseCode.InvalidCredentials: {
+        return redirect('/login');
+      }
+      default: {
+        return data.data;
+      }
+    }
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
   },
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
