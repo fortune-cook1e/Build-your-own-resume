a43d2c1401c6db12d3c49f9810b7f4f7f302caaa
fortune_cookie <767077147@qq.com>
2024-02-16 21:15:20 +0800

    test(web): app web add unit tests

---
+++ apps/web/src/utils/__test__/styles.test.ts
@@ -0,0 +1,19 @@
+import { mergeTailwindCss } from '@/web/utils/styles';
+
+describe('Test Merge TailwindCss', () => {
+  it('add different classname', () => {
+    expect(mergeTailwindCss('class1', 'class2')).toBe('class1 class2');
+  });
+
+  it('add same classname', () => {
+    expect(mergeTailwindCss('class1', 'class1')).toBe('class1 class1');
+  });
+
+  it('add same attribute classname', () => {
+    expect(mergeTailwindCss('px-1', 'p-3')).toBe('p-3');
+  });
+
+  it('override text color', () => {
+    expect(mergeTailwindCss('text-white', 'text-black')).toBe('text-black');
+  });
+});
