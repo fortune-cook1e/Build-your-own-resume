ba3892dba36445f790e4562bd7ae057494d6aaaa
fortune_cookie <767077147@qq.com>
2024-04-11 20:11:32 +0800

    feat(server): add update user api

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Logger,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import {
+  ErrorMessage,
+  UpdateUserDto,
   UserWithPrivateInfo,
   userListSchema,
   userSchema,
 } from '@fe-cookie/resume-generator-shared';
 import { UseUser } from '@/user/decorators/user.decorator';
 import { JwtGuard } from '@/auth/guards/jwt.guard';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { error } from 'console';
 
 @Controller('user')
 @ApiTags('user')
@@ -16,7 +30,7 @@ export class UserController {
 
   @Get()
   @UseGuards(JwtGuard)
-  async getAllUser() {
+  async getAllUsers() {
     const list = await this.userService.findAll();
     return userListSchema.parse(list);
   }
@@ -25,6 +39,34 @@ export class UserController {
   @UseGuards(JwtGuard)
   async getMe(@UseUser() user: UserWithPrivateInfo) {
     return userSchema.parse(user);
+  }
+
+  @Post('update')
+  @UseGuards(JwtGuard)
+  async updateUser(
+    @UseUser('email') email: string,
+    @Body() data: UpdateUserDto,
+  ) {
+    try {
+      if (email !== data.email) {
+        await this.userService.updateByEmail(email, {
+          emailVerified: false,
+          email: data.email,
+        });
+
+        // Todo: send email verfification
+        return await this.userService.updateByEmail(email, {
+          name: data.name,
+          username: data.username,
+        });
+      }
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
+        throw new BadRequestException(ErrorMessage.UserAlreadyExists);
+      }
+      Logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
   }
 
   @Get(':id')
