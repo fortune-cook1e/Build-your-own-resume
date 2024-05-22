bd67d216046f403d3375d7e8538d2677b5b76828
fortune_cookie <767077147@qq.com>
2024-05-22 21:02:44 +0800

    perf(web): extend printing resume request time to 60 seconds

--- apps/web/src/apis/resume/print.ts
+++ apps/web/src/apis/resume/print.ts
@@ -5,7 +5,7 @@ import { Resume } from 'shared';
 
 const printResume = (data: Resume): Promise<{ url: string }> =>
   request.post('/resume/print', data, {
-    timeout: 1000 * 40,
+    timeout: 1000 * 60,
   });
 
 export const usePrintResume = () => {
