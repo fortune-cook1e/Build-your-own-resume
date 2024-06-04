d3b2ec5dad19c83e4b261b2eddc6bff51faf0e6d
fortune_cookie <767077147@qq.com>
2024-06-04 22:11:52 +0800

    chore(web): change resendEmail request timeout

--- apps/web/src/apis/auth/resend-email.ts
+++ apps/web/src/apis/auth/resend-email.ts
@@ -2,7 +2,13 @@ import request from '@/utils/request';
 import { useMutation } from '@tanstack/react-query';
 
 const resendEmail = (email: string) =>
-  request.post('/auth/verify-email/resend', { email });
+  request.post(
+    '/auth/verify-email/resend',
+    { email },
+    {
+      timeout: 1000 * 60,
+    },
+  );
 
 export const useResendEmail = () => {
   const { isPending: loading, mutateAsync: resendEmailFn } = useMutation({
