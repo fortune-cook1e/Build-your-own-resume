ef6676c26cdd34c159e840da001bbc9999dc100a
fortune_cookie <767077147@qq.com>
2024-06-11 13:07:12 +0800

    fix: fix UI component library styles being overwritten

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -5,6 +5,6 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    // './node_modules/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
 };
